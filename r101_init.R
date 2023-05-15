df6e531d00dd9f6baa3e4d95171e81727ed22e71
Diego Villa <ddevildiego@gmail.com>
2023-05-15 18:00:48 -0500

    Non interactive installation

--- r101_init.R
+++ r101_init.R
@@ -1,7 +1,13 @@
-x <- read.csv("https://raw.githubusercontent.com/healthinnovation/upch_r101/main/list_pckg.csv", stringsAsFactors = F)
+url_packages = "https://raw.githubusercontent.com/healthinnovation/upch_r101/main/list_pckg.csv"
 
-pckg <- unlist(x)
+packages_df = read.csv(url_packages, stringsAsFactors = FALSE)
 
-install.packages(pckg)
+packages = unlist(packages_df)
 
-rm(x, pckg)
+options(install.packages.compile.from.source = "always")
+
+install.packages(packages, dependencies = TRUE)
+
+options(install.packages.compile.from.source = "interactive")
+
+rm(url_packages, packages_df, packages)
